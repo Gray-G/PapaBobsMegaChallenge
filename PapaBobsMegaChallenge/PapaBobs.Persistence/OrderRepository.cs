ff8f7cd2ea7aaa8d093f94416d44ab86e222778f
Gray-G <graygolden@gmail.com>
2017-05-06 15:12:58 -0700

    Commit Solution with Separation of Concern layers and working Db
    Commits the .Web, .Domain, .Persistence and .DTO layers. Sets up the SQL Server Database table and ADO.NET Entity model. Contains test code to verify that all the layers communicate correctly and that changes can be saved to the database.

---
+++ PapaBobsMegaChallenge/PapaBobs.Persistence/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Persistence
+{
+    public class OrderRepository
+    {
+        public static void CreateOrder()
+        {
+            var db = new PapaBobsDbEntities();
+
+            // Code for db connection testing purposes
+            var order = new Order();
+            order.OrderId = Guid.NewGuid();
+            order.Size = 1;
+            order.Crust = 0;
+            order.Pepperoni = true;
+            order.Name = "Test";
+            order.Address = "123 Main St";
+            order.Zip = "12345";
+            order.Phone = "123-2345";
+            order.PaymentType = 0;
+            order.TotalCost = 16.50M;
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+        }
+    }
+}

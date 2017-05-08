09c9d97f46a30655d1518148b4e0553772b8fc99
Gray-G <graygolden@gmail.com>
2017-05-08 16:06:08 -0700

    Commit enums
    Implements enumerations to be used with the SQL Server Database.

--- PapaBobsMegaChallenge/PapaBobs.Persistence/OrderRepository.cs
+++ PapaBobsMegaChallenge/PapaBobs.Persistence/OrderRepository.cs
@@ -15,14 +15,14 @@ namespace PapaBobs.Persistence
             // Code for db connection testing purposes
             var order = new Order();
             order.OrderId = Guid.NewGuid();
-            order.Size = 1;
-            order.Crust = 0;
+            order.Size = DTO.Enums.SizeType.Large;
+            order.Crust = DTO.Enums.CrustType.Regular;
             order.Pepperoni = true;
             order.Name = "Test";
             order.Address = "123 Main St";
             order.Zip = "12345";
             order.Phone = "123-2345";
-            order.PaymentType = 0;
+            order.PaymentType = DTO.Enums.PaymentType.Cash;
             order.TotalCost = 16.50M;
 
             db.Orders.Add(order);
